168a2ffaef37428268b93494593d71082639c176
tatsuyaogusu <tatsuyaogusu@gmail.com>
2013-07-29 01:18:46 +0900

    delete unnecessary files

--- build/ofxKinectExample/Build/Intermediates/ofxKinectExample.build/Debug/ofxKinectExample.build/Objects-normal/i386/cameras.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/cameras.c \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/freenect_internal.h \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/libfreenect.h \
-  ../../../addons/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/libfreenect-registration.h \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/usb_libusb10.h \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/registration.h \
-  /Users/tatsuyaogusu/Documents/openFrameworks/of_0071/apps/myApps/Kosekinect/../../../addons/ofxKinect/libs/libfreenect/cameras.h
